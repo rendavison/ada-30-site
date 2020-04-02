0fc9560f8a254855ae2fbc48e28790d6a7ac40e5
Remy Davison <remydavison@remys-mbp-2.lan>
2020-04-02 11:37:10 -0400

    fixed mobile nav so menu starts closed

--- src/js/mobile_nav.js
+++ src/js/mobile_nav.js
@@ -2,9 +2,9 @@ var hamburger = document.getElementById('mobile-nav');
 var links = document.getElementById('main-links');
 
 hamburger.addEventListener('click', function(){
-  if (links.classList.contains('hide')) {
-    links.classList.remove('hide')
+  if (links.classList.contains('show')) {
+    links.classList.remove('show')
   } else {
-    links.classList.add('hide')
+    links.classList.add('show')
   }
 })
